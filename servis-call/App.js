47cc7b93dbbd1a1c4e631a3efb9fd60960a2d44a
Furkan Tufan <furakntufan>
2021-08-15 14:05:10 +0300

    örnek service-call çağırımı eklendi
    axios ile örnek servis çağırımı eklenmiştir.

---
+++ servis-call/App.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import { StyleSheet,SafeAreaView,View, Text, Button } from 'react-native';
+
+import axios from 'react-native-axios';
+
+export default class App extends Component {
+	state = {
+		name: '',
+		surname: '',
+		loading: true
+	};
+
+	componentDidMount() {
+		this.getRandomUser();
+	}
+
+	getRandomUser = () => {
+		this.setState({
+			loading: true,
+		});
+
+		axios
+			.get('https://randomuser.me/api/')
+			.then(user => user.data.results[0])
+			.then(user => {
+				this.setState({
+					name: user.name.first,
+					surname: user.name.last,
+					loading: false
+				});
+			})	
+	};
+
+	render() {
+		const {name,surname,loading} = this.state;
+		return (
+			<SafeAreaView style={styles.container}>
+				<View>
+					{
+						loading ? <Text style={styles.text}>Loading...</Text> :
+						<Text style={styles.text}>{name} {surname}</Text>
+					}
+					<Button
+						title={"Random User"}
+						onPress={this.getRandomUser} />
+				</View>
+			</SafeAreaView>
+		);
+	}
+}
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	text: {
+		textAlign: 'center'
+	}
+});
